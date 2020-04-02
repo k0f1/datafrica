b0dbb1f11da5b870c5d5d43824d26dd87c3a28cd
Kofi Ofuafor <kofuafor@gmail.com>
2020-04-02 12:50:55 +0100

    Store.js added to commit

---
+++ static/store.js
@@ -0,0 +1,9 @@
+//Google sign in
+//BEGIN Pre-requisites
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js">
+</script>
+
+
+<script src="https://apis.google.com/js/client:platform.js?onload=start">
+</script>
+//END Pre-requisites
