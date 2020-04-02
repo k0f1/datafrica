99c2d1d7baf131b9609e9864780a009c85b4f207
Kofi Ofuafor <kofuafor@gmail.com>
2020-04-02 21:44:13 +0100

    This commit re-establishes button outline for signs

---
+++ static/store.js
@@ -0,0 +1,10 @@
+// Google sign in
+// BEGIN Pre-requisites
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/
+    jquery.min.js">
+</script>
+
+
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+
+// END Pre-requisites
