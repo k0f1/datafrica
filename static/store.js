c2950c3e5f1e1734b260456cd66b3877bdb6d146
Kofi Ofuafor <kofuafor@gmail.com>
2020-04-04 02:21:42 +0100

    Minor style changes and js code

--- static/store.js
+++ static/store.js
@@ -1,10 +1,1 @@
-// Google sign in
-// BEGIN Pre-requisites
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/
-    jquery.min.js">
-</script>
 
-
-<script src="https://apis.google.com/js/platform.js" async defer></script>
-
-// END Pre-requisites
